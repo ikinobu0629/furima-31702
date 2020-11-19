f2351e5b4301bab87a5c078e9d7d8cac987c32ab
ikinobu0629 <73089946+ikinobu0629@users.noreply.github.com>
2020-11-19 14:50:20 +0900

    ユーザー管理機能の実装（バリデーション、ページ遷移）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: '※英字と数字の両方を含めて設定してください' 
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+    validates :first_name
+    validates :family_name
+    validates :first_name_kana
+    validates :family_name_kana
+  end
 end
