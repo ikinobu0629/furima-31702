42faa6c803741e8d67f4bf401be6ca2dd327dfa9
ikinobu0629 <73089946+ikinobu0629@users.noreply.github.com>
2020-11-25 18:50:48 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -19,6 +19,24 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+     if @item.user == current_user
+      render :edit
+     else
+      redirect_to root_path
+     end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+     if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
   
 end
