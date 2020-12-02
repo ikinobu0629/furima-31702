5bcf91aaf949465f7d203ff56701e7975d417515
ikinobu0629 <73089946+ikinobu0629@users.noreply.github.com>
2020-12-02 22:23:21 +0900

    商品購入機能のテストコード実装

---
+++ spec/factories/orders.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order do
+    token              { 'uuuuuuuuuu' }
+    postal_code        { '123-4567' }
+    area_id            { 2 }
+    city               { '大阪市' }
+    address            { '大阪1-1-1' }
+    phone_number       { '00000000000' }
+    building_name      {'ビル'}
+    association :user
+    association :item
+  end
+end
